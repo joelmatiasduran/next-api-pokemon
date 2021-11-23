ada269fcca2f136fc4fcd19890294a127c586f19
joelmatiasduran <joelmatiasduran@gmail.com>
2021-11-22 20:16:50 -0500

    Test Color

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -43,104 +43,115 @@ const HomePage: React.FC<Props> = withPageAuthRequired(
       return (
         <>
           <Layout title="Home | Next.js">
-            <div className="flex flex-col items-center text-center justify-center mb-10">
-              <h1 className="w-full flex-col md:flex-col text-2xl md:text-3xl text-center items-center justify-center p-10 bg-yellow-300 rounded-xl">
-                <Link href="/pokemon/7" passHref>
-                  <motion.a
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    className="items-center justify-center"
-                  >
-                    <Squirtle />
-                  </motion.a>
-                </Link>
-                <span className="text-3xl m-5 font-mono">Welcome!!</span>
-                <br></br>
-                <div className="mt-10">
-                  <span className="cursor-pointer capitalize bg-red-500 border-2 border-transparent hover:bg-yellow-300 hover:border-red-600 text-white hover:text-black p-5 rounded-full duration-300">
-                    {" "}
-                    {user.name}{" "}
-                  </span>
-                </div>
-              </h1>
-              {/* <div className="flex flex-col md:p-10 items-center justify-center text-center transform hover:scale-150 bg-blue-400 hover:bg-transparent hover:border-blue-400 border-2 border-transparent rounded-lg text-black hover:text-blue-400 h-24 w-full md:w-1/3 duration-300">
+            <div className="bg-black">
+              <div className="flex flex-col items-center text-center justify-center mb-10">
+                <motion.div
+                  initial={{
+                    boxShadow: "0px 0px 40px #ffd900 ",
+                  }}
+                  className="w-full flex-col md:flex-col text-2xl md:text-3xl text-center items-center justify-center p-10 bg-yellow-300 rounded-full"
+                >
+                  <Link href="/pokemon/7" passHref>
+                    <motion.a
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      className="items-center justify-center"
+                    >
+                      <Squirtle />
+                    </motion.a>
+                  </Link>
+                  <h1 className="text-3xl m-5 font-mono">Welcome!!</h1>
+                  <br></br>
+                  <div className="mt-10">
+                    <span className="cursor-pointer capitalize bg-red-500 border-2 border-transparent hover:bg-yellow-300 hover:border-red-600 text-white hover:text-black p-5 rounded-full duration-300">
+                      {" "}
+                      {user.name}{" "}
+                    </span>
+                  </div>
+                </motion.div>
+                {/* <div className="flex flex-col md:p-10 items-center justify-center text-center transform hover:scale-150 bg-blue-400 hover:bg-transparent hover:border-blue-400 border-2 border-transparent rounded-lg text-black hover:text-blue-400 h-24 w-full md:w-1/3 duration-300">
                 <h2 className="text-sm m-10 text-center font-bold font-mono">
                   Now that you are logged in, you can see all the pokemons! ;)
                 </h2>
               </div> */}
-            </div>
-            <div className="text-center justify-center md:relativ">
-              <div className="px-14 py-6 md:absolute right-0 z-10">
-                <button className="bg-red-600 hover:bg-yellow-300 duration-300 p-6 text-white hover:text-black rounded-full right-5 transform hover:scale-150 cursor-pointer">
-                  Next
-                </button>
               </div>
-              <div className="px-14 py-6 md:absolute left-0 z-10">
-                <button className="bg-red-600 hover:bg-yellow-300 duration-300 p-6 text-white hover:text-black rounded-full left-5 transform hover:scale-150 cursor-pointer">
-                  Previous
-                </button>
-              </div>
-              <form>
-                <h2 className="font-mono focus-within:text-transparent">
-                  SEARCH YOUR POKEMON!
-                </h2>
-                <div className="relative mr-6 my-2">
-                  <input
-                    type="text"
-                    placeholder="Search your Pokemon!"
-                    onChange={handleChange}
-                    className="border-2 border-red-600 p-4 transform duration-300 ease-in-out focus:ring-8 focus:rounded-full"
-                  />
+              <div className="bg-black h-full">
+                <div className="text-center justify-center md:relative h-full py-16">
+                  <div className="px-14 py-6 md:absolute right-0 z-10">
+                    <button className="bg-red-600 hover:bg-yellow-300 duration-300 p-6 text-white hover:text-black rounded-full right-5 transform hover:scale-150 cursor-pointer">
+                      Next
+                    </button>
+                  </div>
+                  <div className="px-14 py-6 md:absolute left-0 z-10">
+                    <button className="bg-red-600 hover:bg-yellow-300 duration-300 p-6 text-white hover:text-black rounded-full left-5 transform hover:scale-150 cursor-pointer">
+                      Previous
+                    </button>
+                  </div>
+                  <form>
+                    <h2 className="font-mono focus-within:text-transparent">
+                      SEARCH YOUR POKEMON!
+                    </h2>
+                    <div className="relative mr-6 my-2">
+                      <input
+                        type="text"
+                        placeholder="Search your Pokemon!"
+                        onChange={handleChange}
+                        className="border-6 border-red-600 p-4 transform duration-300 ease-in-out focus:ring-8 focus:rounded-full"
+                      />
+                    </div>
+                  </form>
                 </div>
-              </form>
-            </div>
+              </div>
 
-            {/*Displaying Pokemon Info */}
+              {/*Displaying Pokemon Info */}
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-              {filteredPokemons.map((pokemon: PokemonTypes, index: number) => (
-                <motion.li
-                  initial={{ x: "-250vw" }}
-                  animate={{ x: 0 }}
-                  key={index}
-                  className="text-center items-center list-none m-4 p-3"
-                >
-                  {{ pokemon } ? (
-                    <motion.span
-                      initial={{
-                        boxShadow: "0px 0px 40px #ff0000 ",
-                      }}
-                      whileHover={{
-                        scale: 1.1,
-                      }}
-                      className="w-full bg-red-600 hover:bg-yellow-300 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row text-black hover:text-red-600 group"
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 bg-black">
+                {filteredPokemons.map(
+                  (pokemon: PokemonTypes, index: number) => (
+                    <motion.li
+                      initial={{ x: "-250vw" }}
+                      animate={{ x: 0 }}
+                      key={index}
+                      className="text-center items-center list-none m-4 p-3"
                     >
-                      <Link href={`/pokemon/${index + 1}`} passHref>
-                        <a className="flex flex-col p-5 items-center justify-center text-center group">
-                          <span className="capitalize text-xl font-bold font-mono">
-                            {pokemon.name}
-                          </span>
-                          <br></br>
-                          <div className="transform hover:scale-150 duration-300 cursor-pointer">
-                            {!pokemon.image ? (
-                              "Loading . . ."
-                            ) : (
-                              <Image
-                                src={pokemon.image}
-                                height={100}
-                                width={100}
-                                alt={pokemon.name}
-                              />
-                            )}
-                          </div>
-                        </a>
-                      </Link>
-                    </motion.span>
-                  ) : (
-                    "loading . . ."
-                  )}
-                </motion.li>
-              ))}
+                      {{ pokemon } ? (
+                        <motion.span
+                          initial={{
+                            boxShadow: "0px 0px 40px #ff0000 ",
+                          }}
+                          whileHover={{
+                            scale: 1.1,
+                          }}
+                          className="w-full bg-red-600 hover:bg-yellow-300 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row text-black hover:text-red-600 group"
+                        >
+                          <Link href={`/pokemon/${index + 1}`} passHref>
+                            <a className="flex flex-col p-5 items-center justify-center text-center group">
+                              <span className="capitalize text-xl font-bold font-mono">
+                                {pokemon.name}
+                              </span>
+                              <br></br>
+                              <div className="transform hover:scale-150 duration-300 cursor-pointer">
+                                {!pokemon.image ? (
+                                  "Loading . . ."
+                                ) : (
+                                  <Image
+                                    src={pokemon.image}
+                                    height={100}
+                                    width={100}
+                                    alt={pokemon.name}
+                                  />
+                                )}
+                              </div>
+                            </a>
+                          </Link>
+                        </motion.span>
+                      ) : (
+                        "loading . . ."
+                      )}
+                    </motion.li>
+                  )
+                )}
+              </div>
             </div>
           </Layout>
         </>
