bfa617d43bbd376fd24133ec954352c82420710d
joelmatiasduran <joelmatiasduran@gmail.com>
2021-11-28 00:45:10 -0500

    Better loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from "react";
 import lottie from "lottie-web";
+import LottieLoading from "./LottieLoading";
 
 interface Props {}
 
 const Loading: React.FC<Props> = () => {
   const elementottie = useRef<HTMLDivElement>(null);
+  const elementottietwo = useRef<HTMLDivElement>(null);
   useEffect(() => {
     lottie.loadAnimation({
       container: elementottie.current as HTMLDivElement,
@@ -13,16 +15,23 @@ const Loading: React.FC<Props> = () => {
       autoplay: true,
       animationData: require("../public/images/squirtle.json"),
     });
-  }, [elementottie]);
+    lottie.loadAnimation({
+      container: elementottie.current as HTMLDivElement,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../public/images/lottie-loading.json"),
+    });
+  }, [elementottie, elementottietwo]);
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-red-600 flex-1 h-full w-full min-h-screen min-w-full">
+      <div className="flex flex-col items-center justify-center bg-red-500 flex-1 h-full w-full min-h-screen min-w-full">
         <div
           className="elementottie h-60 w-60 items-center justify-center text-center"
           ref={elementottie}
         ></div>
-        <div className="pt-12 flex flex-row items-center justify-center">
-          <h1 className="text-white text-3xl">Loading</h1>
+        <div className="pt-6 flex flex-row items-center justify-center">
+          {/* <h1 className="text-white text-3xl">Loading</h1>
           <button className="animate-spin text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +47,11 @@ const Loading: React.FC<Props> = () => {
                 d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
               />
             </svg>
-          </button>
+          </button> */}
+          <div
+            className="elementottietwo h-60 w-60 items-center justify-center text-center"
+            ref={elementottietwo}
+          ></div>
         </div>
       </div>
     </>
---
+++ components/LottieLoading.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from "react";
+import lottie from "lottie-web";
+import { motion } from "framer-motion";
+
+interface Props {}
+
+const LottieLoading: React.FC<Props> = () => {
+  const elementottie = useRef<HTMLDivElement>(null);
+  const [play, setPlay] = useState<null | boolean>(false);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: elementottie.current as HTMLDivElement,
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      animationData: require("../public/images/lottie-loading.json"),
+    });
+    return () => {
+      lottie.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <div
+          className="elementottie h-60 w-60 items-center justify-center text-center cursor-pointer"
+          ref={elementottie}
+          onMouseEnter={() => lottie.play()}
+          onMouseLeave={() => lottie.pause()}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export default LottieLoading;
